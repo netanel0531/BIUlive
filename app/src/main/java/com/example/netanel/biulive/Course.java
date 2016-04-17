2ca4bea55c62932c903ae3bd031174c9e821c61e
raaya <netanel0531@gmail.com>
2016-04-17 19:46:43 +0300

    new class

---
+++ app/src/main/java/com/example/netanel/biulive/Course.java
@@ -0,0 +1,37 @@
+package com.example.netanel.biulive;
+
+import android.view.View;
+
+
+public class Course {
+    private String courseName;
+    private String courseNumber;
+    private int courseGrade;
+    View.OnClickListener listener;
+
+
+
+    public Course(String courseName, String courseNumber,int courseGrade, View.OnClickListener listener){
+        this.courseName = courseName;
+        this.courseNumber = courseNumber;
+        this.courseGrade = courseGrade;
+        this.listener = listener;
+    }
+    public View.OnClickListener getListener() {
+        return listener;
+    }
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public String getCourseName() {
+
+        return courseName;
+    }
+
+    public String getCourseGrade() {
+
+        return Integer.toString(courseGrade);
+    }
+}
